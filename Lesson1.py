f6488af3dc12e6615f3b568cc6a9ddf962690130
mghamdi86 <36012976+mghamdi86@users.noreply.github.com>
2018-03-26 01:14:24 -0700

    Create Lesson1.py

---
+++ Lesson1.py
@@ -0,0 +1,11 @@
+Variables will teach us how to store values for later use:
+x = 5
+Loops will allow us to do some math on every value in a list:
+print(item) for item in ['milk', 'eggs', 'cheese']
+Conditionals will let us run code if some test is true:
+"Our test is true!" if 1 < 2 else "Our test is false!"
+Math Operators allow us to perform mathematical operations on numbers
+5 + 5 # 10
+5 - 5 # 0
+5 * 5 # 25
+5 / 5 # 1
